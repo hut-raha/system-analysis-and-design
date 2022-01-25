c7287e01f6e22146fd8044908cf615093844d7d9
Amirhosein <amirhosein@blazer.localdomain>
2022-01-25 11:07:29 +0330

    deleted website

--- Phase_7/darsafzar/assets/js/user/messages.js
+++
@@ -1,7 +0,0 @@
-function messageOpenDiv(num) {
-    if (document.getElementById("message-menu" + num).style.display == "none") {
-        document.getElementById("message-menu" + num).style.display = "block";
-    } else {
-        document.getElementById("message-menu" + num).style.display = "none";
-    }
-}
